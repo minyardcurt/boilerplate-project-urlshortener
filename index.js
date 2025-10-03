ed89269ddcb2787417d08ae67ff8e08537ef4bb9
CurtMinyard <cminyard@perseverenow.org>
2025-10-03 14:30:22 -0500

    change in connect mongoose

--- index.js
+++ index.js
@@ -17,14 +17,11 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-}).catch(err => {
-  console.error('MongoDB connection error:', err);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const urlSchema = new mongoose.Schema({
   original_url: { type: String, required: true },
