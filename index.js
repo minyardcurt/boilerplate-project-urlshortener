bcd515e85175a60fd2879a7ef38514b0d702883d
CurtMinyard <cminyard@perseverenow.org>
2025-10-03 14:36:00 -0500

    change in connect mongoose

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.use((req, res, next) => {
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 const urlSchema = new mongoose.Schema({
